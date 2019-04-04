2980d3904828ad11451d766c8a27c73b6fb7a55a
JeanStkng <jjg.castillo@yavirac.edu.ec>
2019-04-04 15:01:49 -0500

    v0.4

--- js/boton.js
+++ js/boton.js
@@ -1,5 +1,6 @@
 $(function () {
 
+//Function that displays the buttons inside the ball.
     $("#Button1").click(function () {
         $('.animated').removeClass('zoomOut');
         $('.animated').toggleClass('zoomIn');
@@ -12,16 +13,41 @@ $(function () {
 
     });
 
-      $("#enterButton").click(function () {
+//Button to display login form.
+
+    $("#enterButton").click(function () {
 
         $("#enterButton").hide(400);
 
-        $(".hexagon").css("transform","rotate(150deg)");
+        $(".hexagon").css("transform","rotate(180deg)");
 
+        /* setTimeout(function(){
+          $('.center').toggleClass('pulse');
+          //$('.center').css('width','10px');
+              },300); */
+              //RECORDAR COLOCAR ESTO EN CASO DE CELULARES
 
+        //Animation for the hexagon to go left.
+        setTimeout(function(){
+          $('.center').css('left','25%');
+              },600);
+        //Animation for the hexagon when login form will be displayed.
+        setTimeout(function(){
+          $('.hexagon').addClass('hidden');
+              },800);
+        setTimeout(function(){
+          $('.hexagon').addClass('hiddenEnd');
+              },1100);
+       
+        setTimeout(function(){
+          //$('#loginForm').toggleClass('slideInLeft');
+          $("#loginForm").show('fadeInLeft');
+              },1200);
+        
 
     }); 
 
+    //Toggle white color if mouse hovers the login words.
     $("#Login").hover(
         function() {
           $( this ).toggleClass('text-white');
@@ -30,21 +56,22 @@ $(function () {
         }
       );
 
-        $(document).mouseup(function(e) 
-        {
-            var container = $("#buttonsContainer");
+    //Hide the buttons that were displayed when the ball button was clicked.
+    $(document).mouseup(function(e) 
+    {
+        var container = $("#buttonsContainer");
 
-            // if the target of the click isn't the container nor a descendant of the container
-            if (!container.is(e.target) && container.has(e.target).length === 0) 
-            {
-                $('.animated').removeClass('zoomIn');
-                $('.animated').toggleClass('zoomOut');
-        
-                $("#Button2").hide(250);
-                $("#Button3").hide(250);
-                $("#Button4").hide(250);
-            }
-        });
+        // if the target of the click isn't the container nor a descendant of the container.
+        if (!container.is(e.target) && container.has(e.target).length === 0) 
+        {
+            $('.animated').removeClass('zoomIn');
+            $('.animated').toggleClass('zoomOut');
+    
+            $("#Button2").hide(250);
+            $("#Button3").hide(250);
+            $("#Button4").hide(250);
+        }
+    });
 
       
 });
